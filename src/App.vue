1e182bc332a346835b454ce2731112a0d1df542d
Megan Reh <megreh@gmail.com>
2020-02-21 11:24:18 -0600

    added placeholder text for professor reviews

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,36 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">
+        <span class="d-block d-lg-none">Professorz</span>
+        <span class="d-none d-lg-block">
+          <i style="font-size: 5em;" class="fas fa-book"></i>
+          <br />
+
+          <span style="font-size: 2em;">Professorz</span>
+        </span>
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#about">About</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <router-view />
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,47 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h1 class="my-4">
+      <small>Professor Reviews</small>
+    </h1>
+
+    <!-- Project One -->
+    <div v-for="professor in professors" class="row">
+      <div class="col-md-1">
+        <div style="text-align: center;">Difficulty</div>
+        <h1 style="text-align: center; vertical-align: text-top;">
+          {{ professor.difficulty }}
+        </h1>
+      </div>
+      <div class="col-md-10">
+        <h3>{{ professor.title }} {{ professor.name }}</h3>
+        <p>
+          Sample professor review text goes here
+        </p>
+        <a class="btn btn-secondary" href="#">See All Reviews</a>
+        <br />
+        <br />
+        <br />
+      </div>
+    </div>
   </div>
 </template>
 
+<style></style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      professors: []
+    };
+  },
+  created: function() {
+    axios.get("/professors").then(response => {
+      this.professors = response.data;
+    });
+  },
+  methods: {}
+};
 </script>
